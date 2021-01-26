0f789468cccd51e11eb530b406cc6d533c174b40
Jeremy Peyrat <prstoad@gmail.com>
2021-01-26 11:30:33 +0100

    first commit

--- App.js
+++ App.js
@@ -1,13 +1,87 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider as PaperProvider } from 'react-native-paper';
+import { Appbar } from 'react-native-paper';
+import { Platform } from 'react-native';
+import { ActivityIndicator, Colors } from 'react-native-paper';
+import { Searchbar } from 'react-native-paper';
+
+const MORE_ICON = Platform.OS === 'ios' ? 'dots-horizontal' : 'dots-vertical';
 
 export default function App() {
+
+  const [showSearch, setShowSearch] = useState(false)
+  const [loading, setLoading] = useState(true);
+
+  const [ searchQuery, setSearchQuery] = useState('');
+
+  const onChangeSearch = query => setSearchQuery(query)
+  setTimeout(() => {
+    setLoading(false);
+  }, 3000)
+
+  const annonces = [
+    {
+      name: "a",
+      prix: 10
+    },
+    {
+      name: "b",
+      prix: 10
+    },
+    {
+      name: "c",
+      prix: 10
+    },
+    {
+      name: "d",
+      prix: 10
+    },
+    {
+      name: "e",
+      prix: 10
+    },
+  ]
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <Appbar.Header>
+       <Appbar.Content title="Le bon coin" subtitle={'Petites annonces'} />
+        <Appbar.Action icon="magnify" onPress={() => {setShowSearch(!showSearch)}} />
+        <Appbar.Action icon={MORE_ICON} onPress={() => {}} />
+      </Appbar.Header>
+      {
+        showSearch && 
+        <View>
+          <Searchbar
+            placeholder="Search"
+            onChangeText={onChangeSearch}
+            value={searchQuery}
+          />
+        </View>
+      }
+      <View style={styles.container}>
+        {
+          loading &&
+          <ActivityIndicator animating={true} color={Colors.red800} size="large"/>
+        }
+        {
+          !loading &&
+          annonces.filter(annonce => {
+            return !searchQuery || searchQuery && annonce.name.includes(searchQuery)
+          }).map(annonce => {
+            return (
+              <View>
+                <Text>{annonce.name}</Text>
+                <Text>{annonce.prix}</Text>
+              </View>
+            )
+          })
+        }
+        <StatusBar style="auto" />
+      </View>
+    </PaperProvider>
   );
 }
 
---
+++ README.md
@@ -0,0 +1,6 @@
+expo init live-demo
+cd live-demo
+expo start --web
+
+(après on suit la doc pour installer react-native-paper)
+
